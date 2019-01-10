b79d8c12cd23e3eb890fcf0d72a3dde9f3a0fe55
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-01-10 02:36:35 -0600

    Create AzFileShare.ps1

---
+++ ems-manager/nestedTemplates/AzFileShare.ps1
@@ -0,0 +1,11 @@
+Param (
+  [Parameter()]
+  [String]$SAKey,
+  [String]$SAName
+)
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module Azure -Confirm:$False
+Import-Module Azure
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SourceSAKey
+$storageContext |  New-AzureStorageShare -Name 'emsAzFileShare'
