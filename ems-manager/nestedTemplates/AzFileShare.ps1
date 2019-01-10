3ea1fcb71f45d0f1f1466aa130d163e2729c5632
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-01-10 03:39:33 -0600

    Update AzFileShare.ps1

--- ems-manager/nestedTemplates/AzFileShare.ps1
+++ ems-manager/nestedTemplates/AzFileShare.ps1
@@ -7,5 +7,5 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module Azure -Confirm:$False
 Import-Module Azure
-$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SourceSAKey
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
 $storageContext |  New-AzureStorageShare -Name 'emsAzFileShare'
