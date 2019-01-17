ce456d0c3d22f89bb2b8e3f4c433968a354d054c
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-01-16 23:45:57 -0600

    Update deployRunbook.ps1

--- SQL/deployRunbook.ps1
+++ SQL/deployRunbook.ps1
@@ -1,33 +1,23 @@
 param (
         [Parameter(Mandatory=$false)]
         [string]
-        $ResourceGroupName='autoems',
-
-        [Parameter(Mandatory=$false)]
-        [string]
-        $SQLTemplateFileUri="https://raw.githubusercontent.com/hanuravim/demo-ems/master/SQL/masterTemplate.json",
-
-        [Parameter(Mandatory=$false)]
-        [string]
-        $SQLParameterFileUri="https://raw.githubusercontent.com/hanuravim/demo-ems/master/SQL/masterTemplate.parameters.json"
-        )
+        $TemplateUri="https://raw.githubusercontent.com/hanuravim/demo-ems/master/SQL/deploy.json"
+)
 
 # Authenticate to Azure if running from Azure Automation
 $ServicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"
-Connect-AzureRmAccount `
+Login-AzureRmAccount `
     -ServicePrincipal `
     -TenantId $ServicePrincipalConnection.TenantId `
     -ApplicationId $ServicePrincipalConnection.ApplicationId `
     -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint | Write-Verbose
 
 # Domain Join credential asset
-$myCredential = Get-AutomationPSCredential -Name 'DomainJoin'
+$DomainCredential = Get-AutomationPSCredential -Name 'DomainAdmin1'
+$LocalCredential = Get-AutomationPSCredential -Name 'LocalAdmin'
 
 # Template parameters
-$Parameters = @{
-    "DomainUsername"=$myCredential.UserName;
-    "DomainPassword"=$myCredential.GetNetworkCredential().Password
-}
+$Parameters = @{"DomainUserName"=$DomainCredential.UserName;"DomainPassword"=$DomainCredential.Password;
+"adminUsername"=$LocalCredential.UserName;"adminPassword"=$LocalCredential.Password}
 
-# Deploy SQL Servers
-New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $SQLTemplateFileUri -TemplateParameterObject $Parameters
+New-AzureRmDeployment -Location 'westus2'  -TemplateFile $TemplateUri  -TemplateParameterObject $Parameters
