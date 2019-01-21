aad0606382ddd6b7480827e6c934a89b4921bc6e
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-01-21 01:04:35 -0600

    Update customScripts.ps1

--- ems-manager/nestedTemplates/customScripts.ps1
+++ ems-manager/nestedTemplates/customScripts.ps1
@@ -1,16 +1,16 @@
 #CREATE AZURE FILE SHARE
 Param (
   [Parameter()]
-  [String]$SAKey='GL9JGze0oCXgOq1j0TiSa8CQ/Rz/Mrlo9c1ZXNaVM84L9bjSj2b3xxAQgzfXfUHD+x44YfsvyzNupYtN29Aynw==',
-  [String]$SAName='emscusbdevemsstr001',
-  [String]$share = 'azrfileshare'
+  [String]$SAKey,
+  [String]$SAName,
+  [String]$share
 )
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module Azure -Confirm:$False
 Import-Module Azure
 $storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
-$storageContext |  New-AzureStorageShare -Name azrfileshare
+$storageContext |  New-AzureStorageShare -Name $share
 
 cmdkey /add:$SAName.file.core.windows.net /user:$SAName /pass:$SAKey
 net use K: \\$SAName.file.core.windows.net\$share /u:$SAName $SAKey /persistent:yes
