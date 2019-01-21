ad9f1ff35b7a08947a0d1cb53ba8a003b2677041
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-01-20 23:07:38 -0600

    Update AzFileShare.ps1

--- ems-manager/nestedTemplates/AzFileShare.ps1
+++ ems-manager/nestedTemplates/AzFileShare.ps1
@@ -1,15 +1,15 @@
 Param (
   [Parameter()]
-  [String]$SAKey
-  [String]$SAName
+  [String]$SAKey='GL9JGze0oCXgOq1j0TiSa8CQ/Rz/Mrlo9c1ZXNaVM84L9bjSj2b3xxAQgzfXfUHD+x44YfsvyzNupYtN29Aynw==',
+  [String]$SAName='emscusbdevemsstr001',
+  [String]$share = 'azrfileshare'
 )
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module Azure -Confirm:$False
 Import-Module Azure
-
-#$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
-#$storageContext |  New-AzureStorageShare -Name 'azurefileshare'
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+$storageContext |  New-AzureStorageShare -Name azrfileshare
 
 cmdkey /add:$SAName.file.core.windows.net /user:$SAName /pass:$SAKey
-net use F: \\$SAName.file.core.windows.net\azurefileshare
+net use K: \\$SAName.file.core.windows.net\$share /u:$SAName $SAKey /persistent:yes
