5a8492a828f8da5159b2a9bb58b7b0a9b9fb9f97
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-01-23 16:05:20 -0600

    Update runbook.ps1

--- ems-manager/runbook.ps1
+++ ems-manager/runbook.ps1
@@ -1,8 +1,6 @@
-param (
-        [Parameter(Mandatory=$false)]
-        [string]
-        $TemplateUri="https://raw.githubusercontent.com/hanuravim/demo-ems/master/ems-manager/deploy.json"
-)
+$AutomationAccount = 'fa-dr-poc-automation'
+$RunbookName = 'AzureFileShare'
+$TemplateUri="https://raw.githubusercontent.com/hanuravim/demo-ems/master/ems-manager/deploy.json"
 
 # Authenticate to Azure if running from Azure Automation
 $ServicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"
@@ -13,11 +11,16 @@ Login-AzureRmAccount `
     -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint | Write-Verbose
 
 # Domain Join credential asset
-$DomainCredential = Get-AutomationPSCredential -Name 'DomainAdmin'
+$DomainCredential = Get-AutomationPSCredential -Name 'DomainAdmin1'
 $LocalCredential = Get-AutomationPSCredential -Name 'LocalAdmin'
 
 # Template parameters
 $Parameters = @{"DomainUserName"=$DomainCredential.UserName;"DomainPassword"=$DomainCredential.Password;
 "adminUsername"=$LocalCredential.UserName;"adminPassword"=$LocalCredential.Password}
 
+#Deploy EMS Manager
 New-AzureRmDeployment -Location 'westus2'  -TemplateFile $TemplateUri  -TemplateParameterObject $Parameters
+
+#Configure Azure File Share
+$AutomationRGNAme = (Get-AzureRmAutomationAccount | Where {$_.AutomationAccountName -like $AutomationAccount}).ResourceGroupName
+Start-AzureRmAutomationRunbook –AutomationAccountName $AutomationAccount –Name $RunbookName -ResourceGroupName $AutomationRGNAme
