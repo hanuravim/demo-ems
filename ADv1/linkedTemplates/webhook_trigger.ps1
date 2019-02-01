0a31479af268b2cea29ddbecec83e4decfaedc11
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-01-31 19:35:40 -0600

    Update webhook_trigger.ps1

--- ADv1/linkedTemplates/webhook_trigger.ps1
+++ ADv1/linkedTemplates/webhook_trigger.ps1
@@ -1,4 +1,4 @@
 $uri = 'https://s1events.azure-automation.net/webhooks?token=z41QWKWfCy0uAlBj7V8WZIwhHQF4SwqtGl%2bkfKzT6oM%3d'
 Invoke-RestMethod -Method POST -Uri $uri
-Start-Sleep 15
+Start-Sleep 180
 shutdown /r
